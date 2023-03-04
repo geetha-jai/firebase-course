55aa9433994ff72bccaf065c20e48088c77d4cda
geetha-jai <126946064+geetha-jai@users.noreply.github.com>
2023-03-04 13:28:59 +0530

    updated

--- src/AppReoprt.jsx
+++ src/AppReoprt.jsx
@@ -1,45 +1,71 @@
-import { useEffect, useState } from "react";
-import { db, auth, storage } from "./config/firebase";
-import {
-  getDocs,
-  collection,
-  addDoc,
-  deleteDoc,
-  updateDoc,
-  doc,
-} from "firebase/firestore";
-import { ref, uploadBytes } from "firebase/storage";
-function report(){
+import React, { useState } from "react";
+import { Bars } from 'react-loader-spinner';
+import { getSchool } from "../../firebase/sfirestore";
+import ReportBox from "./ReportBoxComp";
 
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const [schoolList, setSchoolList] = useState([]);
- const SchoolsCollectionRef = collection(db, "Schools")
-const getSchoolList = async () => {
-    try {
-      const data = await getDocs(SchoolsCollectionRef);
-      console.log(data);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setSchoolList(filteredData);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    getSchoolList();
-  }, []);
 
- return(
-     <div>
-        {schoolList.map((school) => (
-          <div>
-            <h1 style={{ color: school.isAtl ? "green" : "red" }}>
-              {school.title}
-            </h1>
-            </div>))}</div>
-)
-        }
-export default report;
+function SchoolReport() {
+    const[data,setData] =useState([]);
+    const [loaderEnable, setLoaderEnable] = useState(true);
+getSchool()
+.then((docSnaps) =>{
+    const dataArray =[];
+    docSnaps.forEach((docSnap) => {
+        dataArray.push(docSnap.data());
+    });
+    setData(dataArray);
+        setLoaderEnable(false);
+    })
+    .catch((err) => {
+        window.location.reload();
+    });
+    document.title = "schoolDataForm | Digital ATL";
+    return(
+        <div className="container">
+        <link rel="stylesheet" href="/CSS/form.css" />
+        <link rel="stylesheet" href="/CSS/report.css" />
+        <div className="title">SchoolDataReport | Digital ATL</div>
+        <hr />
+        {(loaderEnable)?(
+                <center>
+                    <Bars
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="black"
+                        ariaLabel="loading"
+                        wrapperStyle
+                        wrapperClass
+                    />
+                </center>
+                 ):(console.log("Disabled loader"))}
+                 {
+                 data.map((school,index)  =>{
+                    console.log (school);
+                    return(
+                        <ReportBox
+                        key={index}
+                        id={index}
+                        fname={school.name.firstName}
+                        address={school.name.address}
+                        city={school.name.city}
+                        state={school.name.state}
+                        pincode={school.name.pincode}
+                        country={school.name.country}
+                        inchargefname={school.name.inchargefname}
+                        inchargelname={school.name.inchargelname}
+                        inchargeemail={school.name.inchargeemail}
+                        inchargewht={school.name.inchargewht}
+                        principalfname={school.name.principalfname}
+                        principallname={school.name.principallname}
+                        principalemail={school.name.principalemail}
+                        principalwht={school.name.principalwht}
+                        />
+                    );
+                 })}
+                
+                    </div>
+    );
+                }
+                export default SchoolReport;
+                    
